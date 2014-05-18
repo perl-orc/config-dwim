38cb20228cb704b7ca231195c79cc90a4c89547c
James Edward Daniel <james@codegems.org>
2014-05-18 02:56:45 +0100

    tests for Utility::reduce

--- lib/Config/DWIM/Utility.pm
+++ lib/Config/DWIM/Utility.pm
@@ -14,6 +14,30 @@ sub with_only_keys {
   return {%hash};
 }
 
+sub chunk {
+  my ($arrayref, $size) = @_;
+  return $arrayref unless ($size > 0);
+  return $arrayref unless @$arrayref > $size;
+  my @new;
+  my @working = @$arrayref;
+  while (@working) {
+	my @scratch;
+    for (my $i = 0; $i < $size ; $i++) {
+      last unless @working;
+      push @scratch, shift @working;
+	}
+	push @new, [@scratch];
+  }
+  return [@new];
+}
+
+sub reduce {
+  my ($f, $accum, $args, @rest) = @_;
+  return $accum if !$args;
+  my $ret = $f->($accum, ref($args) eq 'ARRAY' ? @$args : $args);
+  return reduce($f, $ret, @rest);
+}
+
 qw(Doing what I mean usefully)
 __END__
 
@@ -30,3 +54,11 @@ Returns the hashref with the selected @keys removed
 =head2 with_only_keys($hashref, @keys) => HashRef
 
 Returns the hashref with only the selected @keys remaining
+
+=head2 chunk(ArrayRef[Any], $size: Int) => ArrayRef[ArrayRef[Any]]
+
+Splits the arrayref into chunks of maximum $size, returns as an arrayref of arrayrefs
+
+=head2 reduce($f: Function, @args: ArrayRef[ArrayRef[Any]]) => Any
+
+Applies one of @args to $f. Attempts to be smart, that is passing an arrayref passes a list. You can double-arrayref to get around that if you need to.
--- t/utility.t
+++ t/utility.t
@@ -8,17 +8,31 @@ sub without {
 sub with {
   return Config::DWIM::Utility::with_only_keys(@_);
 }
-
+sub chunk {
+  return Config::DWIM::Utility::chunk(@_);
+}
+sub reduce {
+  return Config::DWIM::Utility::reduce(@_);
+}
 my %hash = qw(
   foo bar
   bar baz
   baz quux
   quux foo
 );
-
+my $forchunking = [1..16];
+my $chunked = [
+  [1,2], [3,4], [5,6], [7,8], [9,10], [11,12], [13,14], [15,16],
+];
 is_deeply(without(\%hash,qw(foo bar)), {qw(baz quux quux foo)});
 is_deeply(without(\%hash,qw(pancake)), {%hash});
 is_deeply(with(\%hash, qw(foo bar)), {qw(foo bar bar baz)});
 is_deeply(with(\%hash, qw(baz quux)), {qw(baz quux quux foo)});
+is_deeply(chunk($forchunking,2), $chunked);
+is_deeply(chunk([@$forchunking,17],2), [@$chunked,[17]]);
+is_deeply(chunk([],2),[]);
+is_deeply(chunk(["a"],2),["a"]);
+
+is(reduce(sub{ shift() + shift(); }, 1,2,3,4,5,6,7,8),36);
 
 done_testing;
